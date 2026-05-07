f1c1b90c9510f97251dbd4d438c2e930730961be
Juhi-Vyas <juhi2781@gmail.com>
2026-05-07 16:53:11 +0530

    some changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,32 @@
 import express from 'express'
 import { ENV } from './lib/env.js'
+import path from 'path'
+import { connectDB } from './lib/db.js'
+import cors from 'cors'
+import {serve} from "inngest/express"
+import { functions, inngest } from './lib/inngest.js'
 
 const app = express()
 
-console.log(ENV.PORT)
+const __dirname = path.resolve()
 
-app.listen(ENV.PORT, () => {
-    console.log(`Server is running on port ${ENV.PORT}`)
-})
+//middleware
+app.use(express.json())
+app.use(cors({origin:ENV.CLIENT_URL,credentials: true}))
+
+app.use("/api/inngest", serve({client: inngest, functions}))
+ 
+
+const startServer = async () => {
+    try{
+        await connectDB()
+        app.listen(ENV.PORT, () => {
+        console.log(`Server is running on port ${ENV.PORT}`)
+        })
+    }
+    catch (error){
+        console.log("Error starting the server", error)
+    }
+}
+
+startServer()
