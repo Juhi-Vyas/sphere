187146ecc13a1755059ae48671117159d4f16395
Juhi-Vyas <juhi2781@gmail.com>
2026-05-14 20:34:20 +0530

    file naming

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,14 +6,13 @@ import { Toaster } from "react-hot-toast";
 import DashboardPage from "./pages/DashboardPage";
 import ProblemPage from "./pages/ProblemPage";
 import ProblemsPage from "./pages/ProblemsPage";
-import SessionPage from "./pages/SessionPage";
+import SessionPage from "./pages/TempPage";
 
 function App() {
   const { isSignedIn, isLoaded } = useUser();
 
   // this will get rid of the flickering effect
   if (!isLoaded) return null;
-  console.log("isSignedIn", isSignedIn);
   return (
     <>
       <Routes>
