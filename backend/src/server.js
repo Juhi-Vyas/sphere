646564e2d54b36ce3c9a31b4de99627a8a79b3cc
Juhi-Vyas <juhi2781@gmail.com>
2026-05-15 13:45:18 +0530

    fixed cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -18,7 +18,17 @@ const __dirname = path.resolve();
 // middleware
 app.use(express.json());
 // credentials:true meaning?? => server allows a browser to include cookies on request
-app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://sphere-one-omega.vercel.app",
+      "https://sphere-k8fpdyakq-juhi-vyas-projects.vercel.app",
+    ],
+    credentials: true,
+  })
+);
+
 app.use(clerkMiddleware()); // this adds auth field to request object: req.auth()
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
